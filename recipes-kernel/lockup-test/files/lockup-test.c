a036b069465ade8968b45c9d8b6ac18144c38727
Steven Arnold <steven.arnold@dektech.com.au>
2018-07-02 14:29:10 +1000

    Added sysfs interface to start test
    Separate testing from module loading.

--- recipes-kernel/lockup-test/files/lockup-test.c
+++ recipes-kernel/lockup-test/files/lockup-test.c
@@ -1,9 +1,11 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kthread.h>
 #include <linux/jiffies.h>
-
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+#include <linux/mutex.h>
+#include <linux/sysfs.h>
 
 /* module parameters */
 #define HARD_DEFAULT (0)
@@ -12,11 +14,11 @@ module_param(hard, int, 0);
 MODULE_PARM_DESC(hard, "Generate hard lock-up, default="
                  __MODULE_STRING(HARD_DEFAULT));
 
-#define LOCKSEC_DEFAULT (60)
-static int locksec = LOCKSEC_DEFAULT;
-module_param(locksec, int, 0);
-MODULE_PARM_DESC(locksec, "Duration of lockup (seconds) , default="
-                 __MODULE_STRING(LOCKSEC_DEFAULT));
+#define DURATION_DEFAULT (60)
+static int duration = DURATION_DEFAULT;
+module_param(duration, int, 0);
+MODULE_PARM_DESC(duration, "Duration of lockup (seconds) , default="
+                 __MODULE_STRING(DURATION_DEFAULT));
 
 #define ONCPU_DEFAULT (0)
 static int oncpu = ONCPU_DEFAULT;
@@ -26,6 +28,7 @@ MODULE_PARM_DESC(oncpu, "Generate lock-up on cpu, default="
 
 struct task_struct *task;
 static spinlock_t spinlock;
+DEFINE_MUTEX(running);
 
 int hard_lockup_task(void *arg)
 {
@@ -41,7 +44,7 @@ int hard_lockup_task(void *arg)
 	 * to end the lockup.
 	 */
 	loops = 0;
- 	stop = jiffies + msecs_to_jiffies(1000);
+	stop = jiffies + msecs_to_jiffies(1000);
 	while(!kthread_should_stop() && time_before(jiffies, stop)) {
 		cpu_relax();
 		loops++;
@@ -55,8 +58,8 @@ int hard_lockup_task(void *arg)
 	spin_lock(&spinlock);
 
 	loops = elapsedms = 0;
- 	stop = jiffies + msecs_to_jiffies(locksec*1000);
-	while(!kthread_should_stop() && (elapsedms < locksec*1000)
+	stop = jiffies + msecs_to_jiffies(duration*1000);
+	while(!kthread_should_stop() && (elapsedms < duration*1000)
 	      && time_before(jiffies, stop)) {
 		cpu_relax();
 		loops++;
@@ -72,6 +75,7 @@ int hard_lockup_task(void *arg)
 	        jiffies_to_msecs(jiffies));
 
 	task = NULL;
+	mutex_unlock(&running);
 	return 0;
 }
 
@@ -80,7 +84,7 @@ int soft_lockup_task(void *arg)
 	unsigned long stop;
 
 	pr_info("jiffies=%u (ms)\n", jiffies_to_msecs(jiffies));
-	stop = jiffies + msecs_to_jiffies(locksec*1000);
+	stop = jiffies + msecs_to_jiffies(duration*1000);
 
 	spin_lock(&spinlock);
 
@@ -94,29 +98,71 @@ int soft_lockup_task(void *arg)
 	        jiffies_to_msecs(jiffies));
 
 	task = NULL;
+
+	mutex_unlock(&running);
+
 	return 0;
 }
 
-static int lockup_init(void)
+static ssize_t start_show(struct kobject *kobj, struct kobj_attribute *attr,
+                      char *buf)
 {
-	pr_info("Lockup duration = %d\n", locksec);
-	pr_info("Lockup on cpu %d\n", oncpu);
-	spin_lock_init(&spinlock);
+        return sprintf(buf, "%d\n", mutex_is_locked(&running));
+}
+
+static ssize_t start_store(struct kobject *kobj, struct kobj_attribute *attr,
+			   const char *buf, size_t count)
+{
+	(void) buf;
+	(void) kobj;
+	(void) attr;
+
+	if (mutex_trylock(&running) == 0) {
+		return -EBUSY;
+	}
+
+	/* ignore data and start test */
 	if (hard) {
 		pr_info("Starting hard-lockup-test kthread\n");
 		task = kthread_create(&hard_lockup_task,
 				      NULL,
-				      "hard-lockup-test%u");
+				      "hard-lockup-test");
 		kthread_bind(task, oncpu);
 		wake_up_process(task);
 	} else {
 		pr_info("Starting soft-lockup-test kthread\n");
 		task = kthread_create(&soft_lockup_task,
 				      NULL,
-				      "soft-lockup-test%u");
+				      "soft-lockup-test");
 		kthread_bind(task, oncpu);
 		wake_up_process(task);
 	}
+
+        return count;
+}
+
+static struct kobject *lt_kobject;
+static struct kobj_attribute start_attribute =
+	__ATTR(start, 0660, start_show,  start_store);
+
+static int lockup_init(void)
+{
+	int error;
+
+	spin_lock_init(&spinlock);
+	mutex_init(&running);
+
+	lt_kobject = kobject_create_and_add("lockup-test", kernel_kobj);
+	if(!lt_kobject) return -ENOMEM;
+
+	error = sysfs_create_file(lt_kobject, &start_attribute.attr);
+	if (error) {
+		pr_err("failed to create the file /sys/kernel/lockup-test/start\n");
+	}
+
+	pr_info("Lockup duration = %d\n", duration);
+	pr_info("Lockup on cpu %d\n", oncpu);
+	pr_info("Write to /sys/kernel/lockup-test/start to trigger lock-up\n");
 
 	return 0;
 }
@@ -127,6 +173,9 @@ static void lockup_exit(void)
 		pr_info("Stopping lockup-test kthread\n");
 		kthread_stop(task);
 	}
+
+	kobject_put(lt_kobject);
+
 	pr_info("done\n");
 }
 
